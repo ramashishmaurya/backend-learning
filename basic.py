f9458cfd519f07b6a7684a5f2c1a02b348776b8c
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-09-15 22:42:58 +0530

    dict

--- basic.py
+++ basic.py
@@ -22,3 +22,43 @@ dt = list(map(get_email , users))
 print(dt)
 
 
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/datas")
+def information():
+    return({
+        "datainfomation" : "getting the datainfomation"
+    })
+
+
+from pydantic import BaseModel , EmailStr
+
+class Groupsdata(BaseModel):
+    username : str 
+    emails : EmailStr
+
+class Items(BaseModel):
+    description: str | None = None
+    price : float 
+
+
+listn = ["acb" , "abc" , "cde" , "dec"]
+
+def Groupsanagrama(listn): 
+    result = {}
+
+    for i in  listn:
+        key = ''.join(sorted(i))
+
+        if key not in result:
+
+            result[key] = []  
+        
+        result[key].append(i)
+    return(list(result.values()))
+
+
+
